efef29c2ff0112585b19bed03dd4a2718b5568a3
podgaietska <podgaietska.m@gmail.com>
2024-03-09 18:12:58 -0700

    fix userhousehold-service

--- backend/userhousehold-service/src/main/java/com/choresync/userhousehold/UserhouseholdServiceApplication.java
+++ backend/userhousehold-service/src/main/java/com/choresync/userhousehold/UserhouseholdServiceApplication.java
@@ -2,10 +2,12 @@ package com.choresync.userhousehold;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserhouseholdServiceApplication {
 
