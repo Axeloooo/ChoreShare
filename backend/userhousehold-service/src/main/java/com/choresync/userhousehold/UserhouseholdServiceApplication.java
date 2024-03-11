04ba7b5c03285dee55da41fd9c333d8c63fcc939
Axeloooo <axelshz@gmail.com>
2024-03-11 00:05:58 -0600

    feat: K8s configuration files created

--- backend/userhousehold-service/src/main/java/com/choresync/userhousehold/UserhouseholdServiceApplication.java
+++ backend/userhousehold-service/src/main/java/com/choresync/userhousehold/UserhouseholdServiceApplication.java
@@ -2,13 +2,14 @@ package com.choresync.userhousehold;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserhouseholdServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserhouseholdServiceApplication.class, args);
 	}
